a07dde45570f9696335f8dee94816ae5082405a0
thienth32 <thienth32@gmail.com>
2019-01-06 09:29:12 +0700

    update menu for themes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class HomeController extends Controller
 {
-    public function index(){
-    	$products = Product::paginate(10);
-    	return view('homepage', compact('products'));
-    }
+	public function index(){
+		$menus = Category::take(4)->get();
+		$products = Product::take(12)->get();
+		return view('homepage', compact('products', 'menus'));
+	}
 }
