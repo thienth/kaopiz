f17ceb1f253654f524c9202d2d24571ed075cb97
thienth32 <thienth32@gmail.com>
2019-01-12 11:30:44 +0700

    update admin layout

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     public function index(){
-    	return view('layouts.admin.master');
+    	return view('admin.dashboard.index');
     }
 }
---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin.master')
+@section('title', 'Trang quản trị')
+@section('content')
+    Hello dashboard
+    
+@endsection    
